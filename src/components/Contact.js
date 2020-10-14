3a07fd1707a0a7cfba003328809b5bc08ec20dae
Aymeric <aymeric.bouault@gmail.com>
2020-10-14 09:51:30 +0200

    affichage d'une liste de composants

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types'
 
-function Contact (){
-const name = 'Terry Cruz'
+function Contact (props){
+ const name = 'Terry Cruz'
 const online = true;
 const statonline = <span className="status-online"></span>
-const statoffline = <span className="status-offline"></span>
+const statoffline = <span className="status-offline"></span> 
 
     return(
         <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/women/5.jpg"></img>
+            <img className="avatar" src={props.avatar}></img>
             <div>
-            <span className="name">{name}</span>
+            <span className="name">{props.name}</span>
             <div className="status">
                 <span>{online ? statonline : statoffline}</span>
                 <p className="status-text">{online ? "online" : "offline"}</p>
